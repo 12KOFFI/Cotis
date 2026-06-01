690ab959b9439161d2247801d45b17ced3b388ad
Isaac <isaacndri5@gmail.com>
2026-06-01 01:32:29 +0000

    boot

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        
+        $middleware->validateCsrfTokens(except: [
+            'api/webhooks/*',
+        ]);
 
         $middleware->web(append: [
             HandleAppearance::class,
